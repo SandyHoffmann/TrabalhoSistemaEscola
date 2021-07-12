52ead34aac67999b60381ca00cbfc995f729add2
Sandy <manglesan98@gmail.com>
2021-07-11 21:50:14 -0300

    Implementando a classe Aluno

--- js/aluno.js
+++ js/aluno.js
@@ -1,10 +1,45 @@
 export class Aluno {
-    constructor(matricula, nome) {
-        this.matricula = matricula
-        this.nome = nome
-    }
-    dizOi(par) {
-        console.log(par)
-    }
-}
+    #matricula
+    #nota
+    #media
+    constructor(matricula, nome, telefone, email, notas=[], media) {
+        this.#matricula = matricula;
+        this.#nota = notas;
+        this.#media = media;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
 
+    }
+    editarInformacoes(nome, telefone, email) {
+        if (!nome == this.nome) {
+        this.nome = nome;
+        }
+        if (!telefone == this.telefone) {
+        this.telefone = telefone;
+        }
+        if (!email == this.email) {
+        this.email = email;
+        }
+    }
+    calculaMedia() {
+        let soma = 0;
+        for (let x = 0; x < this.notas.length; x++) {
+        soma += this.notas[x];
+        }
+        let media = soma / this.notas.length;
+        this.#media = media
+    }
+
+    cadastraNotas(n1, n2, n3) {
+        this.#nota.push(n1)
+        this.#nota.push(n2)
+        this.#nota.push(n3)
+        this.calculaMedia()
+    }
+
+    get matricula(){
+        return this.#matricula
+    }
+    
+}
